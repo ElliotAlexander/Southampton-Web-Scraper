700fe8fca58353f3523f635b822cccf04586e57f
ElliotAlexander <epurvis97@gmail.com>
2016-10-18 13:58:52 +0100

    Moved everything into static main, for conciseness.

--- SCChallengeEmail.java
+++ SCChallengeEmail.java
@@ -5,28 +5,20 @@ import java.net.URL;
 
 public class SCChallengeEmail {
 
-    private String emailID, line, nameLine;
-    private BufferedReader br;
-
     public static void main(String[] args) throws IOException {
-        SCChallengeEmail mainObject = new SCChallengeEmail();
-        mainObject.run();
-    }
-
-    private void run() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter your email ID:");
-        emailID = br.readLine();
+        String emailID = br.readLine();
         br = new BufferedReader(new InputStreamReader((new URL("http://www.ecs.soton.ac.uk/people/" + emailID).openStream())));
+        String line, nameLine;
         while ((line = br.readLine()) != null) {
             if (line.contains("property=\"name\"")) {
                 nameLine = line.split("name\">")[1].split("<")[0];
                 System.out.println("Name: " + nameLine);
+                System.exit(0);
             }
         }
-        if (nameLine == null) {
-            System.out.println("User not found. ");
-        }
+        System.out.println("User not found.");
     }
 }
 
